f8e95aa17f05242d3698a9883a0be83d9df690ed
Parviz Deyhim <deyhim@deyhim-macbookpro.roam.corp.google.com>
2019-01-20 19:08:37 -0800

    Committing CurrentTimeSampler

--- src/main/java/com/mapr/synth/samplers/CurrentTimeSampler.java
+++ src/main/java/com/mapr/synth/samplers/CurrentTimeSampler.java
@@ -4,12 +4,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import com.mapr.synth.FancyTimeFormatter;
 
-public class IncreasingDateSampler extends FieldSampler {
-    FancyTimeFormatter df = new FancyTimeFormatter();
+public class CurrentTimeSampler extends FieldSampler {
+    FancyTimeFormatter df = new FancyTimeFormatter("yyyy-MM-dd HH:mm:ss");
 
     @Override
     public JsonNode sample() {
         long epoch = System.currentTimeMillis();
-        return TextNode();
+        return new TextNode(df.format(epoch));
     }
 }
