c9cdb03ede14df3b0dabd25360715e9b382126e5
Parviz Deyhim <deyhim@deyhim-macbookpro.roam.corp.google.com>
2019-01-20 22:02:39 -0800

    added sinsimpler

--- src/main/java/com/mapr/synth/samplers/SinSampler.java
+++ src/main/java/com/mapr/synth/samplers/SinSampler.java
@@ -1,4 +1,15 @@
 package com.mapr.synth.samplers;
 
-public class SinSampler {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.DoubleNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+public class SinSampler extends FieldSampler {
+    @Override
+    public JsonNode sample() {
+        long epoch = System.currentTimeMillis();
+        double sinValue = Math.sin(epoch/1000);
+
+        return new DoubleNode(sinValue + 2);
+    }
 }
